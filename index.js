f5c478cbc66e85775b3974b9623da0ddb1f83e1b
Pedro  Paulo Lima Dias <pedroppld@gmail.com>
2024-08-01 20:02:36 -0300

    /

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 
 const corsOptions = {
   origin: (origin, callback) => {
-    const allowedOrigins = ['http://localhost:3000','http://localhost:3001', 'https://radani.vercel.app','https://adm-radani.vercel.app', 'https://radani-conf.vercel.app/']
+    const allowedOrigins = ['http://localhost:3000','http://localhost:3001', 'https://radani.vercel.app','https://adm-radani.vercel.app', 'https://radani-conf.vercel.app']
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
