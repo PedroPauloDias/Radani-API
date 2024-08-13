97e23f4a7e18f714f03e3881818ffb5ab304324c
Pedro  Paulo Lima Dias <pedroppld@gmail.com>
2024-08-12 23:17:11 -0300

    cors

--- routes/products.js
+++ routes/products.js
@@ -5,19 +5,6 @@ const cloudinary = require("../utils/cloudinary");
 const router = express.Router();
 
 
-const corsOptions = {
-  origin: (origin, callback) => {
-    const allowedOrigins = ['http://localhost:3000','http://localhost:3001', 'https://radani.vercel.app','https://adm-radani.vercel.app', 'https://radani-conf.vercel.app']
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Não permitido CORS'));
-    }
-  },
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
 
 
 router.createProduct = async (req, res, next) => {
