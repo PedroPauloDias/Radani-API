375d50e6586b486428eae823faab8c33337d82ff
Pedro  Paulo Lima Dias <pedroppld@gmail.com>
2024-08-13 11:51:07 -0300

    produto / router

--- routes/products.js
+++ routes/products.js
@@ -73,208 +73,87 @@ router.get('/', async (req, res) => {
 });
 
 
-router.get('/:query', async (req, res) => {
-  const query = req.params.query;
-  let page = parseInt(req.query.page) || 1; // Página atual, padrão é 1
-  const pageSize = parseInt(req.query.pageSize) || 10; // Tamanho da página, padrão é 10
+// router.get('/:query', async (req, res) => {
+//   const query = req.params.query;
+//   let page = parseInt(req.query.page) || 1; // Página atual, padrão é 1
+//   const pageSize = parseInt(req.query.pageSize) || 10; // Tamanho da página, padrão é 10
 
-  try {
-    let produtosQuery;
-    let totalProdutos;
+//   try {
+//     let produtosQuery;
+//     let totalProdutos;
 
-    if (!query) {
-      return res.status(400).json({ message: 'A consulta não pode ser vazia' });
-    }
+//     if (!query) {
+//       return res.status(400).json({ message: 'A consulta não pode ser vazia' });
+//     }
 
-    // Tenta encontrar produtos pelo ref
-    [produtosQuery, totalProdutos] = await Promise.all([
-      Produto.find({ ref: query }).sort({ ref: 1 }).skip((page - 1) * pageSize).limit(pageSize),
-      Produto.countDocuments({ ref: query })
-    ]);
+//     // Tenta encontrar produtos pelo ref
+//     [produtosQuery, totalProdutos] = await Promise.all([
+//       Produto.find({ ref: query }).sort({ ref: 1 }).skip((page - 1) * pageSize).limit(pageSize),
+//       Produto.countDocuments({ ref: query })
+//     ]);
 
-    // Se não encontrou produtos pelo ref, tenta buscar por nome ou tag
-    if (produtosQuery.length === 0) {
-      const regexQuery = new RegExp(query, 'i');
-      [produtosQuery, totalProdutos] = await Promise.all([
-        Produto.find({
-          $or: [
-            { name: { $regex: regexQuery } },
-            { tag: { $regex: regexQuery } }
-          ]
-        }).skip((page - 1) * pageSize).limit(pageSize),
-        Produto.countDocuments({
-          $or: [
-            { name: { $regex: regexQuery } },
-            { tag: { $regex: regexQuery } }
-          ]
-        })
-      ]);
-    }
+//     // Se não encontrou produtos pelo ref, tenta buscar por nome ou tag
+//     if (produtosQuery.length === 0) {
+//       const regexQuery = new RegExp(query, 'i');
+//       [produtosQuery, totalProdutos] = await Promise.all([
+//         Produto.find({
+//           $or: [
+//             { name: { $regex: regexQuery } },
+//             { tag: { $regex: regexQuery } }
+//           ]
+//         }).skip((page - 1) * pageSize).limit(pageSize),
+//         Produto.countDocuments({
+//           $or: [
+//             { name: { $regex: regexQuery } },
+//             { tag: { $regex: regexQuery } }
+//           ]
+//         })
+//       ]);
+//     }
 
-    // Verifica se encontrou produtos
-    if (produtosQuery.length === 0) {
-      return res.status(404).json({ message: `Nenhum produto encontrado com a busca '${query}'` });
-    }
+//     // Verifica se encontrou produtos
+//     if (produtosQuery.length === 0) {
+//       return res.status(404).json({ message: `Nenhum produto encontrado com a busca '${query}'` });
+//     }
 
-    // Calcula o número total de páginas
-    const totalPages = Math.ceil(totalProdutos / pageSize);
+//     // Calcula o número total de páginas
+//     const totalPages = Math.ceil(totalProdutos / pageSize);
 
-    // Verifica se a página solicitada é válida
-    if (page < 1) {
-      page = 1;
-    } else if (page > totalPages) {
-      page = totalPages;
-    }
+//     // Verifica se a página solicitada é válida
+//     if (page < 1) {
+//       page = 1;
+//     } else if (page > totalPages) {
+//       page = totalPages;
+//     }
 
-    // Retorna os produtos encontrados e informações de paginação
-    return res.json({
-      produtos: produtosQuery,
-      totalPages,
-      currentPage: page,
-      totalItems: totalProdutos,
-      nextPage: page < totalPages ? page + 1 : null,
-      prevPage: page > 1 ? page - 1 : null
-    });
+//     // Retorna os produtos encontrados e informações de paginação
+//     return res.json({
+//       produtos: produtosQuery,
+//       totalPages,
+//       currentPage: page,
+//       totalItems: totalProdutos,
+//       nextPage: page < totalPages ? page + 1 : null,
+//       prevPage: page > 1 ? page - 1 : null
+//     });
 
-  } catch (error) {
-    console.error("Erro ao buscar produtos por query:", error);
-    // Retorna um erro 500 em caso de falha na consulta
-    return res.status(500).json({ message: "Erro ao buscar produtos por query" });
-  }
-});
-
-
-// Rota para buscar um produto pelo ID
-router.get('/:id', async (req, res) => {
-  const id = req.params.id;
-
-  try {
-    const produto = await Produto.findById(id);
-    ;
-
-    if (!produto) {
-      return res.status(404).json({ message: "Produto não encontrado pelo ID" });
-    }
-
-    // Retorna o produto encontrado
-    res.json(produto);
-
-  } catch (error) {
-    console.error("Erro ao buscar produto pelo ID:", error);
-    // Retorna um erro 500 em caso de falha na consulta
-    res.status(500).json({ message: "Erro ao buscar produto pelo ID" });
-  }
-});
+//   } catch (error) {
+//     console.error("Erro ao buscar produtos por query:", error);
+//     // Retorna um erro 500 em caso de falha na consulta
+//     return res.status(500).json({ message: "Erro ao buscar produtos por query" });
+//   }
+// });
 
 
-
-
-router.get('/produtos/busca/:query', async (req, res) => {
-  const query = req.params.query;
-  let page = parseInt(req.query.page) || 1; // Página atual, padrão é 1
-  const pageSize = parseInt(req.query.pageSize) || 10; // Tamanho da página, padrão é 10
-
-  try {
-    let produtosQuery;
-    let totalProdutos;
-
-    if (!query) {
-      return res.status(400).json({ message: 'A consulta não pode ser vazia' });
-    }
-
-    // Tenta encontrar produtos pelo ref
-    [produtosQuery, totalProdutos] = await Promise.all([
-      Produto.find({ ref: query }).sort({ ref: 1 }).skip((page - 1) * pageSize).limit(pageSize),
-      Produto.countDocuments({ ref: query })
-    ]);
-
-    // Se não encontrou produtos pelo ref, tenta buscar por nome ou tag
-    if (produtosQuery.length === 0) {
-      const regexQuery = new RegExp(query, 'i');
-      [produtosQuery, totalProdutos] = await Promise.all([
-        Produto.find({
-          $or: [
-            { name: { $regex: regexQuery } },
-            { tag: { $regex: regexQuery } }
-          ]
-        }).skip((page - 1) * pageSize).limit(pageSize),
-        Produto.countDocuments({
-          $or: [
-            { name: { $regex: regexQuery } },
-            { tag: { $regex: regexQuery } }
-          ]
-        })
-      ]);
-    }
-
-    // Verifica se encontrou produtos
-    if (produtosQuery.length === 0) {
-      return res.status(404).json({ message: `Nenhum produto encontrado com a busca '${query}'` });
-    }
-
-    // Calcula o número total de páginas
-    const totalPages = Math.ceil(totalProdutos / pageSize);
-
-    // Verifica se a página solicitada é válida
-    if (page < 1) {
-      page = 1;
-    } else if (page > totalPages) {
-      page = totalPages;
-    }
-
-    // Retorna os produtos encontrados e informações de paginação
-    return res.json({
-      produtos: produtosQuery,
-      totalPages,
-      currentPage: page,
-      totalItems: totalProdutos,
-      nextPage: page < totalPages ? page + 1 : null,
-      prevPage: page > 1 ? page - 1 : null
-    });
-
-  } catch (error) {
-    console.error("Erro ao buscar produtos por query:", error);
-    // Retorna um erro 500 em caso de falha na consulta
-    return res.status(500).json({ message: "Erro ao buscar produtos por query" });
-  }
-});
-
-
-// Rota para buscar um produto pelo ID
-router.get('/:id', async (req, res) => {
-  const id = req.params.id;
-
-  try {
-    const produto = await Produto.findById(id);
-    ;
-
-    if (!produto) {
-      return res.status(404).json({ message: "Produto não encontrado pelo ID" });
-    }
-
-    // Retorna o produto encontrado
-    res.json(produto);
-
-  } catch (error) {
-    console.error("Erro ao buscar produto pelo ID:", error);
-    // Retorna um erro 500 em caso de falha na consulta
-    res.status(500).json({ message: "Erro ao buscar produto pelo ID" });
-  }
-});
-
-
-
-
-
-// Rota para buscar um produto pelo ID
-// app.get('/produtos/:id', async (req, res) => {
+// // Rota para buscar um produto pelo ID
+// router.get('/:id', async (req, res) => {
 //   const id = req.params.id;
 
 //   try {
 //     const produto = await Produto.findById(id);
+//     ;
 
 //     if (!produto) {
-//       return res.status(404).json({ message: "Produto não encontrado" });
+//       return res.status(404).json({ message: "Produto não encontrado pelo ID" });
 //     }
 
 //     // Retorna o produto encontrado
@@ -286,6 +165,127 @@ router.get('/:id', async (req, res) => {
 //     res.status(500).json({ message: "Erro ao buscar produto pelo ID" });
 //   }
 // });
+
+
+
+
+router.get('/busca/:query', async (req, res) => {
+  const query = req.params.query;
+  let page = parseInt(req.query.page) || 1; // Página atual, padrão é 1
+  const pageSize = parseInt(req.query.pageSize) || 10; // Tamanho da página, padrão é 10
+
+  try {
+    let produtosQuery;
+    let totalProdutos;
+
+    if (!query) {
+      return res.status(400).json({ message: 'A consulta não pode ser vazia' });
+    }
+
+    // Tenta encontrar produtos pelo ref
+    [produtosQuery, totalProdutos] = await Promise.all([
+      Produto.find({ ref: query }).sort({ ref: 1 }).skip((page - 1) * pageSize).limit(pageSize),
+      Produto.countDocuments({ ref: query })
+    ]);
+
+    // Se não encontrou produtos pelo ref, tenta buscar por nome ou tag
+    if (produtosQuery.length === 0) {
+      const regexQuery = new RegExp(query, 'i');
+      [produtosQuery, totalProdutos] = await Promise.all([
+        Produto.find({
+          $or: [
+            { name: { $regex: regexQuery } },
+            { tag: { $regex: regexQuery } }
+          ]
+        }).skip((page - 1) * pageSize).limit(pageSize),
+        Produto.countDocuments({
+          $or: [
+            { name: { $regex: regexQuery } },
+            { tag: { $regex: regexQuery } }
+          ]
+        })
+      ]);
+    }
+
+    // Verifica se encontrou produtos
+    if (produtosQuery.length === 0) {
+      return res.status(404).json({ message: `Nenhum produto encontrado com a busca '${query}'` });
+    }
+
+    // Calcula o número total de páginas
+    const totalPages = Math.ceil(totalProdutos / pageSize);
+
+    // Verifica se a página solicitada é válida
+    if (page < 1) {
+      page = 1;
+    } else if (page > totalPages) {
+      page = totalPages;
+    }
+
+    // Retorna os produtos encontrados e informações de paginação
+    return res.json({
+      produtos: produtosQuery,
+      totalPages,
+      currentPage: page,
+      totalItems: totalProdutos,
+      nextPage: page < totalPages ? page + 1 : null,
+      prevPage: page > 1 ? page - 1 : null
+    });
+
+  } catch (error) {
+    console.error("Erro ao buscar produtos por query:", error);
+    // Retorna um erro 500 em caso de falha na consulta
+    return res.status(500).json({ message: "Erro ao buscar produtos por query" });
+  }
+});
+
+
+// Rota para buscar um produto pelo ID
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const produto = await Produto.findById(id);
+    ;
+
+    if (!produto) {
+      return res.status(404).json({ message: "Produto não encontrado pelo ID" });
+    }
+
+    // Retorna o produto encontrado
+    res.json(produto);
+
+  } catch (error) {
+    console.error("Erro ao buscar produto pelo ID:", error);
+    // Retorna um erro 500 em caso de falha na consulta
+    res.status(500).json({ message: "Erro ao buscar produto pelo ID" });
+  }
+});
+
+
+
+
+
+ //Rota para buscar um produto pelo ID
+router.get('/produtos/:id', async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const produto = await Produto.findById(id);
+
+    if (!produto) {
+      return res.status(404).json({ message: "Produto não encontrado" });
+    }
+
+    // Retorna o produto encontrado
+    res.json(produto);
+
+  } catch (error) {
+    console.error("Erro ao buscar produto pelo ID:", error);
+    // Retorna um erro 500 em caso de falha na consulta
+    res.status(500).json({ message: "Erro ao buscar produto pelo ID" });
+  }
+});
 
 
 // app.get("/produtos/:id", async (req, res) => {
