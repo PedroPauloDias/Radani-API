aed863c6db9c1783ea5eca99175ac1410489178e
Pedro  Paulo Lima Dias <pedroppld@gmail.com>
2024-08-21 15:13:39 -0300

    rotas categorias/tag

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json()); // Para manipular JSON
 app.use(express.urlencoded({ extended: true })); 
 const corsOptions = {
   origin: (origin, callback) => {
-    const allowedOrigins = ['http://localhost:3000','http://localhost:3001/', 'https://radani.vercel.app','https://adm-radani.vercel.app', 'https://radani-conf.vercel.app']
+    const allowedOrigins = ['http://localhost:3000','http://localhost:3001', 'https://radani.vercel.app','https://adm-radani.vercel.app', 'https://radani-conf.vercel.app']
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
